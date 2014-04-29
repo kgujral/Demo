56805ebad5472fdaace19ee4348f269f5a04c8d3
Neha Singh <neha.singh@prontoitlabs.com>
2014-04-29 12:16:08 +0530

    i have made changes 1st

--- common/BinarySearch.java
+++ common/BinarySearch.java
@@ -1,16 +1,20 @@
 public Class BinarySearch {
 
 	public void inputNumbers() {
-
-abcd by sandeep
+		System.out.print("enter the number here:")
+		abcd by sandeep
 	}
 
 	public void sort() {
+		sorting will be done here by neha
 
 	}
 
 	public void search() {
-search by sandeep
+		search by sandeep
+		searchin procedure comes here
+
+
 	}
 
 }
